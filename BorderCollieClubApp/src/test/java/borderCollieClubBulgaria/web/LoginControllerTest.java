d642ead77f6d6c98a6440f5ede49f1eff0c4d940
Nikolay Bachvarov <nikolai_ba4@abv.bg>
2023-04-08 18:19:35 +0300

    Add files via upload
    69% line Coverage

---
+++ BorderCollieClubApp/src/test/java/borderCollieClubBulgaria/web/LoginControllerTest.java
@@ -0,0 +1,41 @@
+package borderCollieClubBulgaria.web;
+
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class LoginControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void testRegistrationPageShown() throws Exception {
+        mockMvc.perform(get("/users/login")).
+                andExpect(status().isOk()).
+                andExpect(view().name("auth-login"));
+
+    }
+
+    @Test
+    void test_Login_Fail_Returns_Redirect() throws Exception {
+        mockMvc.perform(post("/login-error")
+                        .param("username", "").
+                        param("password", "").
+                        with(csrf())).
+                andExpect(status().is3xxRedirection()).
+                andExpect(redirectedUrl("http://localhost/users/login"));
+
+    }
+}
