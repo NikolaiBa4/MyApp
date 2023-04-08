b4c6bfe636fbf80381f49e3ac897169437d26edf
Nikolay Bachvarov <nikolai_ba4@abv.bg>
2023-04-08 18:43:10 +0300

    69% Line Coverage

---
+++ BorderCollieClubApp/src/test/java/borderCollieClubBulgaria/web/AdminControllerTest.java
@@ -0,0 +1,41 @@
+package borderCollieClubBulgaria.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithAnonymousUser;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class AdminControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    @WithAnonymousUser
+    void get_All_Users_by_Anonymous_redirectsToLogin() throws Exception {
+
+        mockMvc.perform(get("/pages/admins/allUsers"))
+                .andDo(print())
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrlPattern("**/login"));
+    }
+
+    @Test
+    @WithMockUser
+    void get_All_Users_Success() throws Exception {
+
+        mockMvc.perform(get("/pages/admins/allUsers"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(view().name("users"));
+    }
+}
